a448c69936d7e4b3f53d83ca5ec762d3820214ef
ariel <arieljberenstein@gmail.com>
2018-07-25 11:04:41 -0300

    this version allow both, vcf and gvcf modes. Also it acepts a single o multiple bam file path like /path/to_files/*.bam

--- gatk/gvcf_from_bam.py
+++ gatk/gvcf_from_bam.py
@@ -5,10 +5,12 @@ import os
 
 def get_args():
     parser = argparse.ArgumentParser(description='given a list of bam files, get the corresponding gvcfs (one by file) and then a poblational gvcf. This script take advantage of gatk docker')
-    parser.add_argument('-i','--input_dir',required=True)
+    parser.add_argument('-i','--input_files',required=True)
     parser.add_argument('-o','--output_dir',required=True) 
     parser.add_argument('-R','--ReferenceFile',required=False, default = '/home/ariel/Projects/BIA/VCFs/data/hgref/human_g1k_v37_decoy.fasta')
-    parser.add_argument('-l', '--logfile',required = False, default = './out.log')
+    parser.add_argument('-l', '--logfile',required = False, default = None)
+    parser.add_argument('-m', '--mode',required = False, default = 'vcf',help = ' Either "vcf" or "gvcf" ')
+
     # parse args
     args = parser.parse_args()
     return args
@@ -17,8 +19,6 @@ def create_output_dirs(opath):
     if not os.path.exists(opath):
         os.mkdir(opath)
 
-def get_inputfiles(input_dir):
-    return glob.glob('%s*.bam'%input_dir)
 
 import io
 import time
@@ -36,36 +36,54 @@ def popen_with_logging(cmd,logfile = 'out.log'):
 
 
 
-def gatk_docker_get_cmd(basename_file_input, input_dir, output_dir, ReferenceFile, image = 'broadinstitute/gatk'):
+def gatk_docker_get_cmd(input_file, output_dir, ReferenceFile, image = 'broadinstitute/gatk',mode = 'vcf'):
+
+    # output file extension
+    if mode == 'vcf':
+        extension = '.vcf'
+    elif mode == 'gvcf':
+        extension = '.g.vcf'
+    else: 
+        print 'mode must be either, vcf or gvcf. EXIT'
+        exit
+
     # Reference genonme manipulation and mounting
     ReferenceDir = os.path.dirname(ReferenceFile) # get reference path. It must contain reference.fastafai and reference.dict files. 
     ReferenceFile_basename = os.path.basename(ReferenceFile)
 
 
     #Create Docker local paths
-    docker_input_data = '/gatk/input_data/'
+    basename_input_file = os.path.basename(input_file)
+    docker_input_file = '/gatk/%s'%basename_input_file
+    docker_input_file_index = '/gatk/%s.bai'%basename_input_file
+
     docker_dir_output = '/gatk/output/'
     docker_ReferenceDir = '/gatk/refDir/'
     docker_ReferenceFile = '/gatk/refDir/%s'%ReferenceFile_basename 
 
 
     ## mounting commands for docker
-    mount_input_dir = os.path.abspath(input_dir)+':'+docker_input_data
+    mount_input_file = os.path.abspath(input_file)+':'+docker_input_file
     mount_output_dir = os.path.abspath(output_dir)+':'+docker_dir_output
     mount_ref_file = os.path.abspath(ReferenceFile)+':'+docker_ReferenceFile
     mount_ref_dir = ReferenceDir+':'+docker_ReferenceDir  
 
+    mount_input_file_index = os.path.abspath(input_file)+'.bai'+':'+docker_input_file_index
+
     cmd = ['docker', 'run',
-    '-v',mount_input_dir,
+    '-v',mount_input_file,
+    '-v',mount_input_file_index, 
     '-v',mount_output_dir,
     '-v',mount_ref_file,
     '-v',mount_ref_dir,
     '-t',image,
     'gatk','--java-options',"-Xmx4g",'HaplotypeCaller',
     '-R',docker_ReferenceFile,
-    '-I',docker_input_data + basename_file_input,
-    '-O',docker_dir_output + basename_file_input +'.g.vcf',
-    '-ERC', 'GVCF'] 
+    '-I',docker_input_file,
+    '-O',docker_dir_output + basename_input_file + extension]
+
+    if mode == 'gvcf':
+        cmd = cmd +['-ERC', 'GVCF'] 
 
     return cmd
 
@@ -73,20 +91,25 @@ def gatk_docker_get_cmd(basename_file_input, input_dir, output_dir, ReferenceFil
 
 def main():
     args = get_args()
-    input_dir, output_dir, ReferenceFile, logfile = args.input_dir, args.output_dir, args.ReferenceFile, args.logfile
+    input_files, output_dir, ReferenceFile, logfile, mode = args.input_files, args.output_dir, args.ReferenceFile, args.logfile, args.mode
     
     create_output_dirs(output_dir) # no es necesario porque el monatje al docker te lo crea si no existe
-    inputfiles = get_inputfiles(input_dir)
-
+    inputfiles = glob.glob(input_files)
+    print '%s mode activated\n'%mode
+    filestorun = "\n"+"\n".join(inputfiles)
+    print 'you will run gatk on files: %s \n'%filestorun
 
     for ifile in inputfiles:
-        basename_file_input = os.path.basename(ifile)
-        
+        basename_input_file = os.path.basename(ifile)
+        print 'runing %s\n'%basename_input_file
+
         #create docker call
-        cmd = gatk_docker_get_cmd(basename_file_input,input_dir, output_dir, ReferenceFile, image = 'broadinstitute/gatk')
+        cmd = gatk_docker_get_cmd(input_file=ifile, output_dir = output_dir, ReferenceFile = ReferenceFile, image = 'broadinstitute/gatk',mode = mode)
     
         #call docker
-        popen_with_logging(cmd,logfile=output_dir + basename_file_input +'_log.out')
+        if logfile is None:
+            logfile = output_dir + basename_input_file +'.log.out'
+        popen_with_logging(cmd,logfile=logfile)
 
 
 
