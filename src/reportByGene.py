f7dbfda502c887e6378271a9509ecc4bd88b9b8b
ariel <arieljberenstein@gmail.com>
2018-08-03 14:49:58 -0300

    fixing bug hg19 ref

--- src/reportByGene.py
+++ src/reportByGene.py
@@ -357,6 +357,8 @@ def get_exons(genelist,ref,outpath,entrez = False, write_bedfile = True):
 
     print '\n'
     exonbed = pd.concat(exonbed)
+    if ref == 19:
+        exonbed.chrom = exonbed.chrom.apply(lambda x: 'chrm'+str(x))
     
     # write file
     exonbedfile = outpath+'exons_loci.bed'
