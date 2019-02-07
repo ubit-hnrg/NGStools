705c1aced420ce2b5d3467c0c51b9b01f7cb2e72
ubit-hnrg <arieljberenstein@gmail.com>
2019-02-07 16:43:33 -0300

    cleaning

--- pipeline/v1.4/runPipeline2.bash
+++
@@ -1,6 +0,0 @@
-#bash wesPipeline_partial.bash d  EB214 /home/bitgenia/samples/data/EB214/EB214_S3_R1_001.fastq /home/bitgenia/samples/data/EB214/EB214_S3_R2_001.fastq /home/bitgenia/samples/data/EB214/result
-#bash wesPipeline_partial.bash d  EB265 /home/bitgenia/samples/data/EB265/EB265_S5_R1_001.fastq /home/bitgenia/samples/data/EB265/EB265_S5_R2_001.fastq /home/bitgenia/samples/data/EB265/result
-#bash wesPipeline_partial.bash d  EB273 /home/bitgenia/samples/data/EB273/EB273_S9_R1_001.fastq /home/bitgenia/samples/data/EB273/EB273_S9_R2_001.fastq /home/bitgenia/samples/data/EB273/result
-#bash wesPipeline_partial.bash d  EB293 /home/bitgenia/samples/data/EB293/EB293_S12_R1_001.fastq /home/bitgenia/samples/data/EB293/EB293_S12_R2_001.fastq /home/bitgenia/samples/data/EB293/result
-bash wesPipeline.bash d  EB322 /home/bitgenia/samples/data/EB322/EB322_S4_R1_001.fastq /home/bitgenia/samples/data/EB322/EB322_S4_R2_001.fastq /home/bitgenia/samples/data/EB322/result
-#bash wesPipeline_partial.bash d  EB340 /home/bitgenia/samples/data/EB340/EB340_S1_R1_001.fastq /home/bitgenia/samples/data/EB340/EB340_S1_R2_001.fastq /home/bitgenia/samples/data/EB340/result
--- pipeline/v1.4/runPipeline3.bash
+++
@@ -1,4 +0,0 @@
-#bash wesPipeline_partial.bash d  EB273 /home/bitgenia/samples/data/EB273/EB273_S9_R1_001.fastq /home/bitgenia/samples/data/EB273/EB273_S9_R2_001.fastq /home/bitgenia/samples/data/EB273/result
-#bash wesPipeline_partial.bash d  EB293 /home/bitgenia/samples/data/EB293/EB293_S12_R1_001.fastq /home/bitgenia/samples/data/EB293/EB293_S12_R2_001.fastq /home/bitgenia/samples/data/EB293/result
-#bash wesPipeline_partial.bash d  EB322 /home/bitgenia/samples/data/EB322/EB322_S4_R1_001.fastq /home/bitgenia/samples/data/EB322/EB322_S4_R2_001.fastq /home/bitgenia/samples/data/EB322/result
-#bash wesPipeline_partial.bash d  EB340 /home/bitgenia/samples/data/EB340/EB340_S1_R1_001.fastq /home/bitgenia/samples/data/EB340/EB340_S1_R2_001.fastq /home/bitgenia/samples/data/EB340/result
