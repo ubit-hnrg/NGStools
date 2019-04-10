5909bf0e1853eeee88642ce96c93453616f79152
ariel <arieljberenstein@gmail.com>
2019-04-10 12:59:19 -0300

    rename variables

--- pipeline_wdl/qualityControl/BAM_depth.sh
+++ pipeline_wdl/qualityControl/BAM_depth.sh
@@ -12,16 +12,16 @@ $toolpath/bedtools2/bin/intersectBed -a $BAM -b $EXON_coords > $sampleID'_exonTS
 $toolpath/bedtools2/bin/genomeCoverageBed -ibam $sampleID'_exonTSO_reduced.bam' -bga > $sampleID'_exon.coverage'
 
 # Left outer join EXON_coords and BAM coverage. Intermediate file. Incorpora a cada intervalo la covertura, (pero sigue preservando zonas con mínima intersección que traen ruido)
-$toolpath/bedtools2/bin/intersectBed -a $EXON_coords -b $sampleID'_exon.coverage' -loj > loj.txt
+$toolpath/bedtools2/bin/intersectBed -a $EXON_coords -b $sampleID'_exon.coverage' -loj > $sampleID'_loj.txt'
 
 
 #esto limita la covertura estrictamente al intervalo paddeado.
 # Es decicr, esta linea elimina las grandes zonas del bam con cobertura zero que tenían unas pocas bases de interseccion con el intervalo buscado
 # primero: reordeno las columnas de loj.txt, para que las coordenadas start end no sean las del exon, sino las de la cobertura
-awk -F"\t" '{print $1"\t"$7"\t"$8"\t"$9"\t"$4"\t"$5"\t"$2"\t"$3}' loj.txt > loj_sorted_cols.tsv
-$toolpath/bedtools2/bin/intersectBed -a loj_sorted_cols.tsv -b $EXON_coords  > $sampleID'_loj_exon_filtered.coverage'
+awk -F"\t" '{print $1"\t"$7"\t"$8"\t"$9"\t"$4"\t"$5"\t"$2"\t"$3}' $sampleID'_loj.txt' > $sampleID'_loj_sorted_cols.tsv'
+$toolpath/bedtools2/bin/intersectBed -a $sampleID'_loj_sorted_cols.tsv' -b $EXON_coords  > $sampleID'_loj_exon_filtered.coverage'
 
-echo -e 'chr\tstart\tend\tcount\tgeneSymbol\texon_number\texon_start\texon_end' > header
-cat header $sampleID'_loj_exon_filtered.coverage' > $sampleID'_exon_filtered_coverage.tsv'
-rm $sampleID'_loj_exon_filtered.coverage loj.txt' loj_sorted_cols.tsv header $sampleID'_exon.coverage'
+echo -e 'chr\tstart\tend\tcount\tgeneSymbol\texon_number\texon_start\texon_end' > header.tsv
+cat header.tsv $sampleID'_loj_exon_filtered.coverage' > $sampleID'_exon_filtered_coverage.tsv'
+rm $sampleID'_loj_exon_filtered.coverage loj.txt' $sampleID'_loj_sorted_cols.tsv' header.tsv $sampleID'_exon.coverage' $sampleID'_loj.txt'
 
