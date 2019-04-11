71e02990260098fcb91769b7571fe89293f109dd
ariel <arieljberenstein@gmail.com>
2019-04-11 15:02:29 -0300

    minor change

--- pipeline_wdl/annovar/run_annovar.sh
+++ pipeline_wdl/annovar/run_annovar.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 vcf_in=$1
 out_prefix=$2
+
 dbpath=/home/hnrg/HNRG-pipeline-V0.1/dbs/hg19_annovar/
 annovar=/home/bitgenia/dbs/annovar/table_annovar.pl
-perl $perl $vcf_in $dbpath -vcfinput  -buildver hg19 -remove -out $out_prefix -protocol refGene,avsnp150,esp6500siv2_all,1000g2015aug_all,exac03,gnomad_exome,gnomad_genome,clinvar_20180603,intervar_20180118,dbscsnv11,dbnsfp35a,rmsk,tfbsConsSites,cytoBand,wgRna,targetScanS,genomicSuperDups,dgvMerged,gwasCatalog,ensGene,knownGene -operation  g,f,f,f,f,f,f,f,f,f,f,r,r,r,r,r,r,r,r,g,g -nastring . -otherinfo
+perl $annovar $vcf_in $dbpath -vcfinput  -buildver hg19 -remove -out $out_prefix -protocol refGene,avsnp150,esp6500siv2_all,1000g2015aug_all,exac03,gnomad_exome,gnomad_genome,clinvar_20180603,intervar_20180118,dbscsnv11,dbnsfp35a,rmsk,tfbsConsSites,cytoBand,wgRna,targetScanS,genomicSuperDups,dgvMerged,gwasCatalog,ensGene,knownGene -operation  g,f,f,f,f,f,f,f,f,f,f,r,r,r,r,r,r,r,r,g,g -nastring . -otherinfo
