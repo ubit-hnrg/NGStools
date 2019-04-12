4fbf1b2264b86b2139e4dbc069c9a4dce8a63e2f
ariel <arieljberenstein@gmail.com>
2019-04-12 06:13:21 -0300

    merege reports python script

---
+++ pipeline_wdl/qualityControl/merge_sample_reports.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+import sys
+import pandas as pd
+import os
+
+reports = sys.args[1]
+outfile = sys.argv[2]
+merged_report = []
+
+for rep in reports:
+    sample = os.path.basename(rep).split('_')[0]
+    df = pd.read_table(rep,index_col=[0],header =None)
+    df.columns=[sample]
+    merged_report.append(df)
+
+merged_report = pd.concat(merged_report,axis = 1)
+merged_report.index.name=None
+merged_report.to_csv(outfile,sep ='\t')
+
