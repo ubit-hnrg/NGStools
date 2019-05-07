8f2bb8e384d9535ed22aabbb9527ff251b971c17
ubit-hnrg <arieljberenstein@gmial.com>
2019-05-07 10:10:08 -0300

    statndarize_input_generation

--- pipeline_wdl/imputfile_from_multiplexed_fastq/run.sh
+++ pipeline_wdl/imputfile_from_multiplexed_fastq/run.sh
@@ -1,6 +1,20 @@
+
 #/bin/bash
-tso=TSO20190425;
+
+#####################################################################################
+# run as follow: 
+#./run.sh "/home/hnrg/samplesHNRG/TSO20190425/TSO_FONARSEC-125446323/FASTQ_*/*/*.gz"
+
+#### "DO NOT FORGET THE QUOTES!!!!"
+
+#####################################################################################
+
+
+path_to_samples=$1
+tso=$(echo $path_to_samples |cut -f5 -d'/')
+echo 'samples for run '$tso
+
 path=/home/hnrg/executionsHNRG/$tso/inputs/;
 mkdir -p $path;
-readlink -f /home/hnrg/samplesHNRG/$tso/TSO_FONARSEC-125446323/FASTQ_*/*/*.gz|sort | ./generate_fasq_tsv.sh;
+readlink -f $path_to_samples | ./generate_fasq_tsv.sh;
 mv ./samples.txt  $path$tso'_samples.txt'
