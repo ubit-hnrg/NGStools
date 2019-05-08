496fd8499e4abddc771b5c34eb85b11bfb190989
ariel <arieljberenstein@gmail.com>
2019-05-08 10:50:31 -0300

    add vcfanno

--- pipeline_wdl/anotacion_funcional/annot_intervar.sh
+++ pipeline_wdl/anotacion_funcional/annot_intervar.sh
@@ -3,6 +3,7 @@
 vcf=/home/hnrg/resultsHNRG/1711242/1711242.final_annot.vcf
 base=$(basename $vcf)
 DIR=$(readlink -f $(dirname {$file}))
+echo $DIR/$base'_intervar.vcf'
 input=/home/hnrg/resultsHNRG/1711242/1711242_TSO_renamed_one_sample.hg19_multianno.txt
 out=17111242_intervarDB.vcf
 python NGStools/pipeline_wdl/anotacion_funcional/create_InterVarDB.py -i=$input -o $out
@@ -13,4 +14,4 @@ tabix $out.gz
 
 
 #java -Xmx4G -jar /home/bitgenia/samples/HNRG-pipeline-V0.1/tools/SnpSift.jar annotate -v -info InterVarEvidence,InterVarVeredict $out.gz $vcf > $vcf'intervar.vcf'
-/home/hnrg/HNRG-pipeline-V0.1/tools/vcfanno_linux64 -p 4 /home/hnrg/NGStools/pipeline_wdl/anotacion_funcional/intervar_anno_fromVCF.tom $vcf > $DIR/$BASE'_intervar.vcf'
+/home/hnrg/HNRG-pipeline-V0.1/tools/vcfanno_linux64 -p 4 /home/hnrg/NGStools/pipeline_wdl/anotacion_funcional/intervar_anno_fromVCF.tom $vcf > $DIR/$base'_intervar.vcf'
