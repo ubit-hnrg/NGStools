41b4a5d0f7924f98c89fbe91f04df0a5a845c1fc
ariel <arieljberenstein@gmail.com>
2019-05-08 11:29:26 -0300

    add variables

--- pipeline_wdl/anotacion_funcional/annot_intervar.sh
+++ pipeline_wdl/anotacion_funcional/annot_intervar.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 # convert multianno into annovar vcf db (sorted)
-vcf=/home/hnrg/resultsHNRG/1711242/1711242.final_annot.vcf
-base=$(basename $vcf)
+vcfinput=$1
+multianno=$2
+vcfDB='intervar_sample_BD.vcf'
+
+
+base=$(basename $vcfinput)
+BASE="${BASE%.*}"
 DIR=$(readlink -f $(dirname {$file}))
 echo $DIR/$base'_intervar.vcf'
-input=/home/hnrg/resultsHNRG/1711242/1711242_TSO_renamed_one_sample.hg19_multianno.txt
-out=17111242_intervarDB.vcf
-python NGStools/pipeline_wdl/anotacion_funcional/create_InterVarDB.py -i=$input -o $out
+
+# multianno to vcf db file
+python NGStools/pipeline_wdl/anotacion_funcional/create_InterVarDB.py -i=$multianno -o $vcfDB
 
 # index
-bgzip $out
-tabix $out.gz
+bgzip $vcfDB
+tabix $vcfDB.gz
 
-sed -e "s|__vcfDB__|$out.gz|g" /home/hnrg/NGStools/pipeline_wdl/anotacion_funcional/intervar_vcfanno_template_fromVCF.tom > config_vcfanno.tom
-#java -Xmx4G -jar /home/bitgenia/samples/HNRG-pipeline-V0.1/tools/SnpSift.jar annotate -v -info InterVarEvidence,InterVarVeredict $out.gz $vcf > $vcf'intervar.vcf'
-/home/hnrg/HNRG-pipeline-V0.1/tools/vcfanno_linux64 -p 4 config_vcfanno.tom $vcf > $DIR/$base'_intervar.vcf'
+sed -e "s|__vcfDB__|$vcfDB.gz|g" /home/hnrg/NGStools/pipeline_wdl/anotacion_funcional/intervar_vcfanno_template_fromVCF.tom > config_vcfanno.tom
+#java -Xmx4G -jar /home/bitgenia/samples/HNRG-pipeline-V0.1/tools/SnpSift.jar annotate -v -info InterVarEvidence,InterVarVeredict $vcfDB.gz $vcf > $vcf'intervar.vcf'
+/home/hnrg/HNRG-pipeline-V0.1/tools/vcfanno_linux64 -p 4 config_vcfanno.tom $vcfinput > $DIR/$BASE'_intervar.vcf'
