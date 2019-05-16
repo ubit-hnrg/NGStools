1a10f4f74f73268eaecdf004fd6aca630c5be175
ariel <arieljberenstein@gmail.com>
2019-05-16 10:13:04 -0300

    file for deducing bam from gene name

--- bamfiletools/reduceBam_oneGene.sh
+++ bamfiletools/reduceBam_oneGene.sh
@@ -1,29 +1,7 @@
-#! /bin/bash
 #!/bin/bash
-
-while getopts ":i:b:o:s:" opt; do
-  case $opt in
-    i) ivcf="$OPTARG"
-    ;;
-    b) ibam="$OPTARG"
-    ;;
-    o) opath="$OPTARG"
-    ;;
-    s) samplename="$OPTARG"
-    ;;
-    g) gap="$OPTARG"
-    ;;
-    \?) echo "Invalid option -$OPTARG" >&5
-    ;;
-  esac
-done
-
-printf "Argument ivcf is %s\n" "$ivcf"
-printf "Argument ibam is %s\n" "$ibam"
-printf "Argument opath is %s\n" "$opath"
-printf "Argument samplename is %s\n" "$samplename"
-printf "Argument delta is %s\n" "$gap"
-
-#grep -v "^#" $ivcf | awk '{if ($2 > $delta) print $1, $2-$delta-1, $2+$delta-1;  else print $1, 0, $2+$delta-1; }' > $opath/$ivcf.bed
-#samtools view -bh -L "${ivcf}" "${ibam}" > "${opath}"/"${samplename}"_reduced.bam
-#samtools index "${opath}"/"${samplename}"_reduced.bam
+bamfile=$1
+gen=$2;
+sample=$3
+zcat /home/hnrg/HNRG-pipeline-V0.1/libraries/GRCh37/exon_coords_canonical_transcript_GRCh37.tsv.gz |grep -P $gen'\t'|head -n1 |cut -f1,2,3 > $gen.bed
+intersectBed -a $bamfile -b $gen.bed > $sample'_'$gen.bam
+samtools index $sample'_'$gen.bam $sample'_'$gen.bai
