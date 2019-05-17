cbe5f234bdd997146092b77e928275a92051c99c
ariel <arieljberenstein@gmail.com>
2019-05-16 23:04:22 -0300

    harcode realpath exon coverage

--- pipeline_wdl/process_vcf/filter_vcf_by_gene_lists.sh
+++ pipeline_wdl/process_vcf/filter_vcf_by_gene_lists.sh
@@ -23,8 +23,7 @@ python $parseshets -i=/home/hnrg/metadataHNRG/$runID/TSO-$date'-bioinfo.xlsx' -o
 
 #scatter
 for i in "${samples[@]}";
-do 
-    echo $i;
+    do 
     # prepare id  ## This is only for supportting samplenames starting with numbers. (SnpSift compatibility)
     if [[ $i =~ ^[0-9].* ]]
     then
@@ -37,6 +36,7 @@ do
     filtered=/home/hnrg/resultsHNRG/$runID/$i/$i'_geneList'.multiano_multisample.tsv
     variants=/home/hnrg/resultsHNRG/$runID/$i/$i.multiano_multisample.tsv
     output_xlsx=/home/hnrg/resultsHNRG/$runID/$i/$i.variants.xlsx
+    out_bad_genes=/home/hnrg/resultsHNRG/$runID/$i/$i'_NotIn_refGene.txt'
     metadata_path=/home/hnrg/metadataHNRG/$runID/$i/$i
     exon_coverage=$(readlink -f /home/hnrg/executionsHNRG/$runID/cromwell-executions/main_workflow/7b733377-eb59-4ea3-81e7-13f9594d2d5b/call-quality_control/qual_control.quality_control/ffc1d0a9-f582-4a47-a47e-9e46961817cf/call-bam_depth/*/execution/$i'_coverage_statistics_by_exon.tsv')
     
@@ -51,5 +51,5 @@ do
 
 
     #check if any gene do not mach with our list
-    grep $metadata_path'_genelist.txt' -v -f $refGenelist > $metadata_path'NotIn_refGene.txt';
+    grep $metadata_path'_genelist.txt' -v -f $refGenelist > $out_bad_genes;
 done
