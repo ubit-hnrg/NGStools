f2227c99ea6fec806939c45ce651a3dc5b17e9b0
ariel <arieljberenstein@gmail.com>
2019-08-12 12:10:55 -0300

    change to xlsxwriter module. Memory efficient for large datasets

--- pipeline_wdl/qualityControl/make_excel_report.py
+++ pipeline_wdl/qualityControl/make_excel_report.py
@@ -6,18 +6,36 @@ import openpyxl
 
 report_pairs =sys.argv[1:-1]
 output = sys.argv[-1]
+workbook = xlsxwriter.Workbook(output, {'constant_memory': True})  # ROBUSTO PARA GRANDES FILES.
 
-#report_pairs = ['./result_fastp.tsv:Filtrado',\
-#                './TSO20190328_samtools_report.tsv:Alineamiento',\
-#                'TSO20190328_coverage_statistics.tsv:Profundidad']
-#output = 'out.xlsx'
 
 report_names  = [i.split(':')[1] for i in report_pairs]
 report_fnames = [i.split(':')[0] for i in report_pairs]
 
-with pd.ExcelWriter(output) as writer:
-    for report_name, report_fname in zip(report_names,report_fnames):
-        report_table = pd.read_table(report_fname)
-        report_table.to_excel(writer, sheet_name=report_name,index = False)
+
+for report_name, report_fname in zip(report_names,report_fnames):
+    print report_name
+    worksheet = workbook.add_worksheet(report_name)
+    report_table = pd.read_table(report_fname,header = None,encoding='utf-8')
+    
+    shape = report_table.shape
+    row_max = shape[0]
+    col_max = shape[1]
+    columns = report_table.columns
+    index = report_table.index
+    nulls = report_table.isnull()  #para evitar errores escribiendo nulls
+    #write sheet
+    for row in range(0, row_max):
+        for col in range(0, col_max):
+            cell = report_table.iloc[row,col]
+            null = nulls.iloc[row,col]
+            if not null:
+                worksheet.write(row, col, cell)
+workbook.close()
+
+#with pd.ExcelWriter(output) as writer:
+#    for report_name, report_fname in zip(report_names,report_fnames):
+#        report_table = pd.read_table(report_fname)
+#        report_table.to_excel(writer, sheet_name=report_name,index = False)
         
 
