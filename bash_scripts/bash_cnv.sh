bcc6aad8141ac24d1b45e04bb1402c9ed2c6b761
abbutinn <aguizquierdo@gmail.com>
2019-11-15 12:06:02 -0300

    fix bug

--- bash_scripts/bash_cnv.sh
+++ bash_scripts/bash_cnv.sh
@@ -8,13 +8,4 @@ out=$4
 # bed = /home/usuario/bam/intervalo_b37_padded_100.bed
 # fasta = /home/usuario/reference/hs37d5.fa
 ###step1
-sudo Rscript ReadInBams.R --bams $bams --bed $bed --fasta $fasta --out $out/resultado1 \ 
-
-##step2
-Rscript IdentifyFailures.R --Rdata $out/resultado1.RData --mincorr .98 --mincov 100 --out $out/resultado2 \
-
-###step 3
-Rscript makeCNVcalls.R --Rdata $out/resultado1.RData --transProb 0.01 --out $out/resultado3 --plot None \
-
-
-while read line; do name=$(basename $line .bam); head -n1 $out/resultado3_all.txt > $out/$name.txt && grep "$name" $out/resultado3_all.txt >> $out/$name.txt; done < $bams
+sudo Rscript ReadInBams.R --bams $bams --bed $bed --fasta $fasta --out $out/resultado1 && Rscript IdentifyFailures.R --Rdata $out/resultado1.RData --mincorr .98 --mincov 100 --out $out/resultado2 && Rscript makeCNVcalls.R --Rdata $out/resultado1.RData --transProb 0.01 --out $out/resultado3 --plot None && while read line; do name=$(basename $line .bam); head -n1 $out/resultado3_all.txt > $out/$name.txt && grep "$name" $out/resultado3_all.txt >> $out/$name.txt; done < $bams
