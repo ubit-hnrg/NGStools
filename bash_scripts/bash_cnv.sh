2fc9890d833cbbc78e2a030c3f75e425060d1fad
abbutinn <aguizquierdo@gmail.com>
2019-11-15 11:52:08 -0300

    fix bug

--- bash_scripts/bash_cnv.sh
+++ bash_scripts/bash_cnv.sh
@@ -11,10 +11,10 @@ out=$4
 sudo Rscript ReadInBams.R --bams $bams --bed $bed --fasta $fasta --out $out/resultado1 \ 
 
 ##step2
-&& Rscript IdentifyFailures.R --Rdata $out/resultado1.RData --mincorr .98 --mincov 100 --out $out/resultado2 \
+Rscript IdentifyFailures.R --Rdata $out/resultado1.RData --mincorr .98 --mincov 100 --out $out/resultado2 \
 
 ###step 3
-&& Rscript makeCNVcalls.R --Rdata $out/resultado1.RData --transProb 0.01 --out $out/resultado3 --plot None \
+Rscript makeCNVcalls.R --Rdata $out/resultado1.RData --transProb 0.01 --out $out/resultado3 --plot None \
 
 
 && while read line; do name=$(basename $line .bam); head -n1 $out/resultado3_all.txt > $out/$name.txt && grep "$name" $out/resultado3_all.txt >> $out/$name.txt; done < $bams
