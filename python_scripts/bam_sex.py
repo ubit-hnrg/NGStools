c1af2ad8acd9ce8162d31c2491cef37efbabba35
abbutinn <aguizquierdo@gmail.com>
2020-03-30 11:25:28 -0300

    fix bug

--- python_scripts/bam_sex.py
+++ python_scripts/bam_sex.py
@@ -41,7 +41,7 @@ sample = os.path.splitext(os.path.basename(bam))[0]#subprocess.call(["basename",
 a = int(y)/int(x) >= 0.07
 if a >= 0.07: 
     sex = 'M'
-elif a < 0.07 or 0.02 >= a:
+elif a < 0.07 or 0.03 >= a:
     sex = '?'
 else:
     sex = 'F'
