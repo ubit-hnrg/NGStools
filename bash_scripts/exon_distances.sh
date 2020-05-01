e455780b239bcf2d19699ceaeba730fe37f24bf6
abbutinn <aguizquierdo@gmail.com>
2020-04-30 22:21:10 -0300

    fix bug

--- bash_scripts/exon_distances.sh
+++ bash_scripts/exon_distances.sh
@@ -7,10 +7,10 @@ vcf_pwd = $(pwd $vcf)
 exon_file=$2
 
 
-grep "^#" $1 > head_vcf.vcf
-grep -v "#" $1| sort -k1,1 -k2,2n >> head_vcf.vcf
+grep "^#" $vcf > head_vcf.vcf
+grep -v "#" $vcf| sort -k1,1 -k2,2n >> head_vcf.vcf
 
-bedtools closest -a head_vcf.vcf -b $2 -D ref | cut -f 1,2,12-17  > $vcf_pwd/$vcf_name'.exon_distance.tsv'
+bedtools closest -a head_vcf.vcf -b $exon_file -D ref | cut -f 1,2,12-17  > $vcf_pwd/$vcf_name'.exon_distance.tsv'
 
 rm head_vcf.vcf
 
