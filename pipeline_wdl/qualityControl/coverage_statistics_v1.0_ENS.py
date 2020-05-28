5a433b2f86e89c2f981988dae03ce19562f465bf
abbutinn <aguizquierdo@gmail.com>
2020-05-27 22:41:20 -0300

    fix bug

--- pipeline_wdl/qualityControl/coverage_statistics_v1.0_ENS.py
+++ pipeline_wdl/qualityControl/coverage_statistics_v1.0_ENS.py
@@ -26,13 +26,10 @@ output_coverage_by_exon = args.output_exon_report
 
 
 signif=2
-#sample=os.path.basename(ffile).split('_')[0]
-#print sample
-#output_global_coverage = '%s_coverage_statistics.tsv'%sample
-#output_coverage_by_exon = '%s_coverage_statistics_by_exon.tsv'%sample
 
 
 
+####aca creas una funcion para hacer la cuenta para eliminar la covertura 0 del experimento no? 
 def depth_fraction(coverage,thr=0,ZeroDepth=False):
     if not ZeroDepth:
         condition = coverage['count']>=thr
@@ -43,12 +40,11 @@ def depth_fraction(coverage,thr=0,ZeroDepth=False):
     return coverage[condition].count_length.sum()/float(coverage.count_length.sum())
 
 
-coverage = pd.read_table(ffile)
-
 # ensure intervals fall inside library kit
 coverage = coverage[(coverage.chr==coverage.exon_chr)&(coverage.start>=coverage.exon_start)&(coverage.end<=coverage.exon_end)]
 #sort and remove duplicates
 coverage.sort_values(by=['chr','start','end'],ascending=[True,True,False])
+
 coverage.drop_duplicates(inplace = True,subset=['chr','start','geneSymbol','ENSEMBL_ID'])
 
 coverage['count_length']=coverage.end-coverage.start
@@ -62,7 +58,6 @@ bases_greater1 = depth_fraction(coverage,thr=1)
 bases_greater10 = depth_fraction(coverage,thr=10)
 bases_greater20 = depth_fraction(coverage,thr=20)
 bases_greater30 = depth_fraction(coverage,thr=30)
-
 # uso estadisticos pesados popr la longitud de cada intervalo de profundidad constante
 weighted_stats = DescrStatsW(coverage['count'], weights=coverage.count_length, ddof=0)
 
@@ -88,7 +83,6 @@ global_statistics = res.loc[[u'dp>=1',u'dp>=10', u'dp>=20', u'dp>=30',
          u'median_DP', u'q25_DP', u'q75_DP', u'q95_DP',
          u'mean_DP', u'std_DP'],:]
 
-
 # # compute per gene statistics
 
 depth1 = coverage.groupby(['geneSymbol','ENSEMBL_ID','exon_number','strand'])['count','count_length'].apply(lambda x: depth_fraction(x,thr=1))
@@ -97,17 +91,14 @@ depth20 = coverage.groupby(['geneSymbol','ENSEMBL_ID','exon_number','strand'])['
 depth30 = coverage.groupby(['geneSymbol','ENSEMBL_ID','exon_number','strand'])['count','count_length'].apply(lambda x: depth_fraction(x,thr=30))
 
 
+
 depth_by_exon = pd.concat([depth1,depth10,depth20,depth30],axis=1)
 depth_by_exon.columns=['dp1','dp10','dp20','dp30']
 depth_by_exon =depth_by_exon.round(2)
 
-
 exon_description = coverage[['chr','exon_start','exon_end','geneSymbol','ENSEMBL_ID','exon_number','strand']].drop_duplicates()
-depth_by_exon = pd.merge(depth_by_exon,exon_description,how='left',left_index=True,right_on=['geneSymbol','exon_number'])
+
+depth_by_exon = pd.merge(depth_by_exon,exon_description,how='left',left_index=True,right_on=['geneSymbol','ENSEMBL_ID','exon_number','strand'])
 depth_by_exon = depth_by_exon[[u'geneSymbol',u'ENSEMBL_ID', u'exon_number',u'strand', u'dp1', u'dp10', u'dp20', u'dp30', u'chr', u'exon_start', u'exon_end']]
 depth_by_exon.sort_values(by=['chr','exon_start','exon_end'],inplace = True)
-
-
-global_statistics.round(2).to_csv(output_global_coverage,sep='\t')
-depth_by_exon.to_csv(output_coverage_by_exon,index = False,sep='\t')
-
+depth_by_exon.reset_index(inplace=True)
