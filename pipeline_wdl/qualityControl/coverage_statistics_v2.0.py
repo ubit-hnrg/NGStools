2d78734c2a23c411a957a27e61e4a38f33905cc0
abbutinn <aguizquierdo@gmail.com>
2020-09-05 19:35:56 -0300

    fix bug

---
+++ pipeline_wdl/qualityControl/coverage_statistics_v2.0.py
@@ -0,0 +1,122 @@
+#!/usr/bin/python
+from __future__ import division
+
+from statsmodels.stats.weightstats import DescrStatsW
+import sys
+import pandas as pd
+import numpy as np
+import os
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-i','--exon_coverage_regions', help='exon coverage regions in exons')
+parser.add_argument('-g','--output_global_report') #default='global_coverage_statistics.tsv'
+parser.add_argument('-e','--output_exon_report')# default='./coverage_statistics_by_exon.tsv')
+parser.add_argument('-s','--samplename')
+
+args =  parser.parse_args()
+
+
+
+#ffile='EB802_exon_filtered_coverage.tsv.gz'
+#ffile = sys.argv[1]
+ffile = args.exon_coverage_regions  # input file
+sample = args.samplename
+output_global_coverage = args.output_global_report
+output_coverage_by_exon = args.output_exon_report
+
+coverage = pd.read_table(ffile)
+
+signif=2
+
+
+
+####aca creas una funcion para hacer la cuenta para eliminar la covertura 0 del experimento no? 
+def depth_fraction(coverage,thr=0,ZeroDepth=False):
+    if not ZeroDepth:
+        condition = coverage['count']>=thr
+    else:
+        condition = coverage['count']== thr
+
+    
+#    return coverage[condition].count_length.sum()/float(coverage.count_length.sum())
+    #a = coverage[condition].count_length.sum()
+    b = float(coverage.count_length.sum())
+
+    if b!= 0:
+        return coverage[condition].count_length.sum()/b
+    else:
+        return 0
+    #c = np.divide(a,b)
+
+    #return #np.divide(a,b, out=np.zeros_like(a), where=b!=0)#coverage[condition].count_length.sum(),float(coverage.count_length.sum())) # if float(coverage.count_length.sum()) !=0 else 0
+    #return  a/b if b != 0 else 0
+
+
+# ensure intervals fall inside library kit
+coverage = coverage[(coverage.chr==coverage.exon_chr)&(coverage.start>=coverage.exon_start)&(coverage.end<=coverage.exon_end)]
+#sort and remove duplicates
+coverage.sort_values(by=['chr','start','end'],ascending=[True,True,False])
+
+coverage.drop_duplicates(inplace = True,subset=['chr','start','geneSymbol','ENSEMBL_ID'])
+
+coverage['count_length']=coverage.end-coverage.start
+coverage['exon_length']=coverage.exon_end-coverage.exon_start
+
+
+# ## Compute global statistics
+
+bases_without_reads = depth_fraction(coverage,ZeroDepth=True)
+bases_greater1 = depth_fraction(coverage,thr=1)
+bases_greater10 = depth_fraction(coverage,thr=10)
+bases_greater20 = depth_fraction(coverage,thr=20)
+bases_greater30 = depth_fraction(coverage,thr=30)
+# uso estadisticos pesados popr la longitud de cada intervalo de profundidad constante
+weighted_stats = DescrStatsW(coverage['count'], weights=coverage.count_length, ddof=0)
+
+
+global_depth={}
+global_depth.update({'mean_DP':round(weighted_stats.mean,signif)})
+global_depth.update({'median_DP':weighted_stats.quantile(0.5).values[0]})
+global_depth.update({'std_DP':round(weighted_stats.std,signif)})
+global_depth.update({'q25_DP':weighted_stats.quantile(0.25).values[0]})
+global_depth.update({'q75_DP':weighted_stats.quantile(0.75).values[0]})
+global_depth.update({'q95_DP':weighted_stats.quantile(0.95).values[0]})
+
+global_depth.update({'dp>=1':bases_greater1})
+global_depth.update({'dp>=10':bases_greater10})
+global_depth.update({'dp>=20':bases_greater20})
+global_depth.update({'dp>=30':bases_greater30})
+global_depth.update({'q95_DP':weighted_stats.quantile(0.95).values[0]})
+#global_depth.update({'DP=0':round(bases_without_reads,signif)})
+
+res = pd.Series(global_depth).to_frame()
+res.columns=[sample]
+global_statistics = res.loc[[u'dp>=1',u'dp>=10', u'dp>=20', u'dp>=30', 
+         u'median_DP', u'q25_DP', u'q75_DP', u'q95_DP',
+         u'mean_DP', u'std_DP'],:]
+
+# # compute per gene statistics
+
+depth1 = coverage.groupby(['geneSymbol','ENSEMBL_ID','exon_number','strand'])['count','count_length'].apply(lambda x: depth_fraction(x,thr=1))
+depth10 = coverage.groupby(['geneSymbol','ENSEMBL_ID','exon_number','strand'])['count','count_length'].apply(lambda x: depth_fraction(x,thr=10))
+depth20 = coverage.groupby(['geneSymbol','ENSEMBL_ID','exon_number','strand'])['count','count_length'].apply(lambda x: depth_fraction(x,thr=20))
+depth30 = coverage.groupby(['geneSymbol','ENSEMBL_ID','exon_number','strand'])['count','count_length'].apply(lambda x: depth_fraction(x,thr=30))
+
+
+
+depth_by_exon = pd.concat([depth1,depth10,depth20,depth30],axis=1)
+depth_by_exon.columns=['dp1','dp10','dp20','dp30']
+depth_by_exon =depth_by_exon.round(2)
+
+exon_description = coverage[['chr','exon_start','exon_end','geneSymbol','ENSEMBL_ID','exon_number','strand']].drop_duplicates()
+
+depth_by_exon = pd.merge(depth_by_exon,exon_description,how='left',left_index=True,right_on=['geneSymbol','ENSEMBL_ID','exon_number','strand'])
+depth_by_exon = depth_by_exon[[u'geneSymbol',u'ENSEMBL_ID', u'exon_number',u'strand', u'dp1', u'dp10', u'dp20', u'dp30', u'chr', u'exon_start', u'exon_end']]
+depth_by_exon.sort_values(by=['chr','exon_start','exon_end'],inplace = True)
+#depth_by_exon.reset_index(inplace=True)
+
+
+global_statistics.round(2).to_csv(output_global_coverage,sep='\t')
+depth_by_exon.to_csv(output_coverage_by_exon,index = False,sep='\t')
---
+++ pipeline_wdl/qualityControl/estadistica_fastp_V2.py
@@ -0,0 +1,191 @@
+#!/usr/bin/python
+from __future__ import division
+import json
+import pandas as pd
+import os
+import numpy as np
+import argparse
+
+
+###### importar jsons
+#archivo_paths = ["/home/usuario/fastp_results/1711242.txt", "/home/usuario/fastp_results/1802140.txt","/home/usuario/fastp_results/1804642.txt","/home/usuario/fastp_results/1805817.txt","/home/usuario/fastp_results/1809341.txt","/home/usuario/fastp_results/1809720.txt","/home/usuario/fastp_results/1810255.txt","/home/usuario/fastp_results/1901364.txt","/home/usuario/fastp_results/1901981.txt","/home/usuario/fastp_results/EB761.txt","/home/usuario/fastp_results/EB790.txt","/home/usuario/fastp_results/EB802.txt"]
+
+
+
+parser = argparse.ArgumentParser(prog='estadistica_fastp.py',description='Extract statistic info from fastp_json reports', usage='%(prog)s  --fastp_json_report --output_file')
+parser.add_argument('-i','--fastp_json_report_list', help='input file with one_sample_multi_lane_fastpjson_report_file per line')
+parser.add_argument('-o','--output_file', help='a tsv file with all statistics')
+args = parser.parse_args()
+
+
+results_dict = {}
+reportes = []
+
+json_in = args.fastp_json_report_list
+out = args.output_file
+
+ 
+with open(json_in) as fp:
+    content = fp.readlines()
+    content = [x.strip() for x in content] 
+
+######################
+
+
+    array_reports = []
+    total_reads_bef1 = []
+    total_bases_bef1 = []
+    q20_rates_bef1 = [] 
+    q30_rates_bef1 = [] 
+    gc_content_bef = []
+    read1_mean_length_bef1 = []
+    read2_mean_length_bef1 = []
+    total_reads_aft = []
+    total_bases_aft = []
+    q20_rates_aft = [] 
+    q30_rates_aft = []
+    gc_content_aft =[]
+    read1_mean_length_aft = []
+    read2_mean_length_aft = []
+    #####filter_results
+    passed_filter_reads = []
+    low_quality_reads = []
+    too_many_N_reads = []
+    too_short_reads = []
+    too_long_reads = []    
+    total_bases_bef1 = []
+    total_bases_aft = []
+    #results_dict = []
+
+for q in range(len(content)):
+
+
+    
+    lectura_media_r1_bef = 0
+    lectura_media_r2_bef = 0
+    lectura_media_r1_aft = 0
+    lectura_media_r2_aft = 0
+
+
+    
+    
+    #lines = tuple(open(content[q], 'r'))
+    #lenght = len(lines)
+
+
+    
+    jsonfilename=content[q]
+    
+    json=pd.read_json(jsonfilename)
+    
+    #update observables.
+    ####campos importantes before filtering
+    total_reads_bef1.append(json.summary.before_filtering['total_reads'])
+    total_bases_bef1.append(json.summary.before_filtering['total_bases'])
+    gc_content_bef..append(json.summary.before_filtering['gc_content'])
+    q20_rates_bef1.append(json.summary.before_filtering['q20_rate'])
+    q30_rates_bef1.append(json.summary.before_filtering['q30_rate'])
+    read1_mean_length_bef1.append(json.summary.before_filtering['read1_mean_length'])
+    read2_mean_length_bef1.append(json.summary.before_filtering['read2_mean_length'])
+    
+
+    ####campos importantes after filtering
+    total_reads_aft.append(json.summary.after_filtering['total_reads'])
+    total_bases_aft.append(json.summary.after_filtering['total_bases'])
+    gc_content_aft.append(json.summary.after_filtering['gc_content'])
+    q20_rates_aft.append(json.summary.after_filtering['q20_rate'])
+    q30_rates_aft.append(json.summary.after_filtering['q30_rate'])
+    read1_mean_length_aft.append(json.summary.after_filtering['read1_mean_length'])
+    read2_mean_length_aft.append(json.summary.after_filtering['read2_mean_length'])
+    
+
+    ####campos importantes filtering_results
+
+    passed_filter_reads.append(json.filtering_result['passed_filter_reads'])
+    low_quality_reads.append(json.filtering_result['low_quality_reads'])
+    too_many_N_reads.append(json.filtering_result['too_many_N_reads'])
+    too_short_reads.append(json.filtering_result['too_short_reads'])
+    too_long_reads.append(json.filtering_result['too_long_reads'])
+    
+    sample_name=os.path.basename(content[q]).strip('.txt')
+
+
+total_lecturas=sum(total_reads_bef1)
+bases_before = float(sum(total_bases_bef1)/1000000000)
+total_lecturas_passTrue=sum(total_reads_aft)
+bases_after = float(sum(total_bases_aft)/1000000000
+gc_before = sum(gc_content_bef)
+gc_after = sum(gc_content_aft)
+porcentaje_passed = round((total_lecturas_passTrue*100)/total_lecturas,2)
+porcentaje_low_qual = round((sum(low_quality_reads)*100)/total_lecturas,2)
+porcentaje_too_many_N = round((sum(too_many_N_reads)*100)/total_lecturas,2)
+porcentaje_too_short_reads = round((sum(too_short_reads)*100)/total_lecturas,2)
+porcentaje_too_long_reads = round((sum (too_long_reads)*100)/total_lecturas,2)
+
+#############q20 y q30
+#porc_q30_bases_aft = round((sum(q30_bases_aft)*100)/sum(total_bases_aft),2)
+#porc_q20_bases_aft = round((sum(q20_bases_aft)*100)/sum(total_bases_aft),2)
+#porc_q30_bases_bef1 = round((sum(q30_bases_bef1)*100)/sum(total_bases_bef),2)
+#porc_q20_bases_bef1 = round((sum(q20_bases_bef1)*100)/sum(total_bases_bef),2)
+porc_q30_rates_aft = round(np.average(q30_rates_aft,weights=total_bases_aft),2)
+porc_q20_rates_aft = round(np.average(q20_rates_aft,weights=total_bases_aft),2)
+porc_q30_rates_bef1 = round(np.average(q30_rates_bef1,weights=total_bases_bef1),2)
+porc_q20_rates_bef1 = round(np.average(q20_rates_bef1,weights=total_bases_bef1),2)
+
+
+lectura_media_r1_befNP = round(np.average(read1_mean_length_bef1,weights=total_bases_bef1),2)
+lectura_media_r2_befNP = round(np.average(read2_mean_length_bef1,weights=total_bases_bef1),2)
+lectura_media_r1_aftNP = round(np.average(read1_mean_length_aft,weights=total_bases_aft),2)
+lectura_media_r2_aftNP = round(np.average(read2_mean_length_aft,weights=total_bases_aft),2)
+    
+    
+results_dict.update({"total de lecturas antes del filtrado":int(total_lecturas)})
+results_dict.update({"total de GIGAbases antes del filtrado":gc_before})##bases before
+results_dict.update({"total de GIGAbases despues del filtrado":gc_after})##bases_after
+results_dict.update({"total de lecturas despues del filtrado":int(total_lecturas_passTrue)})
+results_dict.update({"total de lecturas despues del filtrado[%]":porcentaje_passed})
+results_dict.update({"lecturas lowqual[%]":porcentaje_low_qual})
+results_dict.update({"lecturas NNNNN[%]":porcentaje_too_many_N})
+results_dict.update({"lecturas muy cortas[%]":porcentaje_too_short_reads})
+results_dict.update({"lecturas muy largas[%]":porcentaje_too_long_reads})
+results_dict.update({"longitud media R1 antes del filtrado":lectura_media_r1_befNP})
+results_dict.update({"longitud media R2 antes del filtrado":lectura_media_r2_befNP})
+results_dict.update({"longitud media R1 despues del filtrado":lectura_media_r1_aftNP})
+results_dict.update({"longitud media R2 despues del filtrado":lectura_media_r2_aftNP})
+results_dict.update({"q30 despues del filtrado[%]":porc_q30_rates_aft})
+results_dict.update({"q20 despues del filtrado[%]":porc_q20_rates_aft})
+results_dict.update({"q30 antes del filtrado[%]":porc_q30_rates_bef1})
+results_dict.update({"q20 antes del filtrado[%]":porc_q20_rates_bef1})
+
+results_dict.update({"contenido GC antes":gc_before})##gc_content before
+results_dict.update({"contenido GC despues":gc_after})##gc_content_after
+
+res=pd.Series(results_dict).to_frame()
+res.columns = [sample_name]
+    #res.index
+res = res.loc[[u'total de lecturas antes del filtrado',
+             u'total de GIGAbases antes del filtrado',
+             u'total de lecturas despues del filtrado',
+             u'total de GIGAbases despues del filtrado',
+             u'total de lecturas despues del filtrado[%]',
+             u'lecturas lowqual[%]',
+             u'lecturas NNNNN[%]',
+             u'lecturas muy cortas[%]',
+             u'lecturas muy largas[%]',
+             u'contenido GC antes',
+             u'contenido GC despues',
+            
+             u'q30 antes del filtrado[%]',
+             u'q30 despues del filtrado[%]',
+             u'q20 antes del filtrado[%]',
+             u'q20 despues del filtrado[%]',
+                         
+             u'longitud media R1 antes del filtrado',
+             u'longitud media R2 antes del filtrado',
+             u'longitud media R1 despues del filtrado',
+             u'longitud media R2 despues del filtrado'],:]
+    
+#pd.options.display.float_format = '{:.2f}'.format
+#display(pd.concat(reportes, axis=1).round(2))
+res.round(2).to_csv(out,sep = '\t',header =False)
+#pd.concat(reportes, axis=1).round(2).to_csv(out,sep='\t')
