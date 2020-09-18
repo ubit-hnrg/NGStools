36fd7c40b8703e105416e2cd417a7c229d796963
abbutinn <aguizquierdo@gmail.com>
2020-09-18 00:50:24 -0300

    fix bug

--- pipeline_wdl/qualityControl/upgrade.sh
+++ pipeline_wdl/qualityControl/upgrade.sh
@@ -10,23 +10,13 @@
         set -e
 
         exp_name=$1 #Nombre Experimento, Ej: TSOxxxxxx
-<<<<<<< HEAD
-        output_path_upgrade = '/data/excecutionHNRG/upgrade_stats/'+ $2 + '/inputs'
+        #output_path_upgrade='/data/excecutionHNRG/upgrade_stats/'$2'/inputs'
           
-        path_data_bam = '/data/resultsHNRG/'+$exp_name+'/*/*[!_haplotype].bam'
-        #path_data_index = /data/resultsHNRG/'+$exp_name+'/*/*
-
-        for f in exp_name; do readlink -f $path_data_bam; done
-
-=======
-        output_path_upgrade='/data/excecutionHNRG/upgrade_stats/'$2'/inputs'
-          
-        path_data='/data/resultsHNRG/'$exp_name'/*/*'
-        input = $3
-dirname($path_data'[!_haplotype].'$input)
->>>>>>> 899e1dfa52e11cada99b6e86e333780c742ada83
+        path_bam='/data/resultsHNRG/'$exp_name'/*/*[!_haplotype].bam'
+        #input = $3
+        #dirname($path_data'[!_haplotype].'$input)
 
 
-        #for f in exp_name; do readlink -f $path_data'/*[!_haplotype].bam;done
+        for f in exp_name; do readlink -f $path_bam;done
 
         
