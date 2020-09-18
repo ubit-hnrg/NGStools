a3776af76e9d62bcbf75e770127db51716099cb5
abbutinn <aguizquierdo@gmail.com>
2020-09-18 01:05:27 -0300

    fix bug

--- pipeline_wdl/qualityControl/upgrade.sh
+++ pipeline_wdl/qualityControl/upgrade.sh
@@ -13,10 +13,13 @@
         #output_path_upgrade='/data/excecutionHNRG/upgrade_stats/'$2'/inputs'
           
         path_bam='/data/resultsHNRG/'$exp_name'/*/*[!_haplotype].bam'
+        path_index = '/data/resultsHNRG/'$exp_name'/*/*[!_haplotype].bai'
+        path_excecution = '/home/hnrg/executionsHNRG/upgrade_stats/'$exp_name
+        path_fasq = '/data/excecutionsHNRG/'$exp_name'/inputs/'$exp_name'.txt'
         #input = $3
         #dirname($path_data'[!_haplotype].'$input)
 
-
-        for f in exp_name; do readlink -f $path_bam;done
+        mkdir -p $path_excecution'/upgrade_stats/inputs'
+        for f in exp_name; do readlink -f $path_bam >> $path_excecution'/inputs/bams.txt'; readlink -f $path_bam >> $path_excecution'/inputs/index.txt';readlink -f $path_fasq; done
 
         
