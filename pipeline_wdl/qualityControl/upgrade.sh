5d0ab0c175d8d901e35339c2c35113630ff1218d
abbutinn <aguizquierdo@gmail.com>
2020-09-18 00:41:10 -0300

    creador de inputs para upgrade_workflow (bams,bams_index,fastq_sample)

--- pipeline_wdl/qualityControl/upgrade.sh
+++ pipeline_wdl/qualityControl/upgrade.sh
@@ -1,11 +1,22 @@
  #!/bin/bash
+
+ ###############33
+ ## bash for upgrade_statistics; collect input mandatory files
+## ##TSOxxxx
+##  bam ###bam;
+##  index #### bai
+# usage = 
+###########
         set -e
 
-        exp_name = $1 #Nombre Experimento, Ej: TSOxxxxxx
+        exp_name=$1 #Nombre Experimento, Ej: TSOxxxxxx
         output_path_upgrade = '/data/excecutionHNRG/upgrade_stats/'+ $2 + '/inputs'
           
-        path_data = '/data/resultsHNRG/'+$exp_name+'/*/*'
+        path_data_bam = '/data/resultsHNRG/'+$exp_name+'/*/*[!_haplotype].bam'
+        #path_data_index = /data/resultsHNRG/'+$exp_name+'/*/*
 
-        for f in exp_name; do readlink -f $path_data'/*[!_haplotype].bam;done
+        for f in exp_name; do readlink -f $path_data_bam; done
+
+
 
         
