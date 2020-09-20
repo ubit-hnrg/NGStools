c0ce30f3c0bc92d4110dca2e17aff74be62bef2e
abbutinn <aguizquierdo@gmail.com>
2020-09-20 03:04:24 -0300

    fix bug

--- pipeline_wdl/qualityControl/upgrade.sh
+++ pipeline_wdl/qualityControl/upgrade.sh
@@ -10,6 +10,10 @@
         set -e
 
         exp_name=$1 #Nombre Experimento, Ej: TSOxxxxxx
+        exp_lib=$2
+        trim_front=$3
+        trim_tail=$4
+
         #output_path_upgrade='/data/excecutionHNRG/upgrade_stats/'$2'/inputs'
           
         path_bam='/data/resultsHNRG/'$exp_name'/*/*[!_haplotype].bam'
@@ -20,6 +24,7 @@
         #dirname($path_data'[!_haplotype].'$input)
 
         mkdir -p $path_excecution'/upgrade_stats/inputs'
-        for f in exp_name; do readlink -f $path_bam >> $path_excecution'/inputs/bams.txt'; readlink -f $path_index >> $path_excecution'/inputs/index.txt';readlink -f $path_fasq; done
+        for f in exp_name; do readlink -f $path_index >> $path_excecution/inputs/bams_index.txt; readlink -f $path_bam >> $path_excecution/inputs/bams.txt; cp $path_fasq $path_excecution/inputs; done
 
-        
+
+        python json_upgrade.py -sa $path_excecution'/inputs/'$1'.txt' -bam $path_excecution'/inputs/bams.txt' -bai $path_excecution'/inputs/bams_index.txt' -tf $3  -tt $4 -e $2 
