f484f115e584523e0940c06fc7ab4a39571faed4
abbutinn <aguizquierdo@gmail.com>
2020-10-10 12:28:02 -0300

    fix bug

--- bash_scripts/cobertura_panel_digital.sh
+++ bash_scripts/cobertura_panel_digital.sh
@@ -12,15 +12,15 @@ panel_name=$(basename $lista_genes .genes)
 
 python /home/hnrg/NGStools/python_scripts/panel_virtual.py -l $lista_genes -ic $intervalo_restricted -o intervalo_panel_digital.bed
 
-/home/hnrg/HNRG-pipeline-V0.1/tools/bedtools2/bin/coverageBed -abam $bam  -b intervalo_panel_digital.bed -hist -sorted > $sample_name'.'$panel_digital.hist.aux1
+/home/hnrg/HNRG-pipeline-V0.1/tools/bedtools2/bin/coverageBed -abam $bam  -b intervalo_panel_digital.bed -hist -sorted > $sample_name.hist.aux1
 echo -e 'chr\tstart\tend\ttranscriptID\tgene\texonNumber\tstrand\tDP\tBPs\tIntervalLength\tfrequency' > header.txt
-grep -v '^all' $sample_name'.'$panel_digital.hist.aux1 > $sample_name'.'$panel_digital.hist.aux2
-cat header.txt $sample_name'.'$panel_digital.hist.aux2 > $sample_name'.'$panel_digital.hist
-rm $sample_name'.'$panel_digital.hist.aux1 $sample_name'.'$panel_digital.hist.aux2 header.txt
+grep -v '^all' $sample_name.hist.aux1 > $sample_name.hist.aux2
+cat header.txt $sample_name.hist.aux2 > $sample_name.hist
+rm $sample_name.hist.aux1 $sample_name.hist.aux2 header.txt
 
 
 # make tsv coverage report by exon
-python /home/hnrg/NGStools/pipeline_wdl/qualityControl/local_coverage_report_ENS_intersect_Library.py -i=$sample_name.$panel_digital.hist -o $sample_name_$panel_digital_coverage.tsv -s=$sample_name
+python /home/hnrg/NGStools/pipeline_wdl/qualityControl/local_coverage_report_ENS_intersect_Library.py -i=$sample_name.hist -o $sample_name'_'$panel_digital_coverage'.tsv' -s=$sample_name
        
 
 
