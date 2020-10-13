793adb610db864d73a81178126c2b7d23d873df1
abbutinn <aguizquierdo@gmail.com>
2020-10-12 21:33:54 -0300

    fix bug

--- bash_scripts/cobertura_panel_digital_nodup.sh
+++ bash_scripts/cobertura_panel_digital_nodup.sh
@@ -13,8 +13,7 @@ panel_name=$(basename $lista_genes .genes)
 python /home/hnrg/NGStools/python_scripts/panel_virtual.py -l $lista_genes -ic $intervalo_restricted -o intervalo_panel_digital.bed -ne $panel_name'_no_encontrado.tsv' 
 
 #### quito duplicados
-##/home/hnrg/HNRG-pipeline-V0.1/tools/samtools-1.9
-#samtools view -F1024 $bam> bam_nodups.bam
+/home/hnrg/HNRG-pipeline-V0.1/tools/samtools-1.9/samtools view -F1024 $bam > bam_nodups.bam
 #/home/hnrg/HNRG-pipeline-V0.1/tools/bedtools2/bin/coverageBed -b bam_nodups.bam  -a intervalo_panel_digital.bed -g /home/hnrg/HNRG-pipeline-V0.1/references/hs37d5/hs37d5.genome -hist -sorted > $sample_name.hist.aux1
 
 ####con dup
@@ -23,13 +22,13 @@ python /home/hnrg/NGStools/python_scripts/panel_virtual.py -l $lista_genes -ic $
 echo -e 'chr\tstart\tend\ttranscriptID\tgene\texonNumber\tstrand\tDP\tBPs\tIntervalLength\tfrequency' > header.txt
 grep -v '^all' $sample_name.hist.aux1 > $sample_name.hist.aux2
 cat header.txt $sample_name.hist.aux2 > $sample_name.hist
-#rm $sample_name.hist.aux1 $sample_name.hist.aux2 header.txt bam_nodups.bam
+rm $sample_name.hist.aux1 $sample_name.hist.aux2 header.txt bam_nodups.bam
 
-rm $sample_name.hist.aux1 $sample_name.hist.aux2 header.txt 
+#rm $sample_name.hist.aux1 $sample_name.hist.aux2 header.txt 
 
 
-# make tsv coverage report by exon
-#python /home/hnrg/NGStools/pipeline_wdl/qualityControl/local_coverage_report_dp_20X.py -i=$sample_name.hist -o $panel_name'_digital_panel_coverage.tsv' -pn=$panel_name
+#make tsv coverage report by exon
+python /home/hnrg/NGStools/pipeline_wdl/qualityControl/global_coverage_report_digital_panel.py -i=$sample_name.hist -o $panel_name'_digital_panel_coverage.tsv' -pn=$panel_name
        
 
 #rm $sample_name.hist
