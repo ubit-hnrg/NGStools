5eea9fff4dc04a12839dbeb00684ad3d49d690df
abbutinn <aguizquierdo@gmail.com>
2021-04-19 15:40:13 -0300

    count barcodes in fastq file

---
+++ python_scripts/barcode-freq.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+import argparse
+import csv, os, sys, gzip, argparse
+from operator import itemgetter
+
+
+
+#parser = argparse.ArgumentParser(prog='barcode-freq.py',description='counts barcodes freq. Prints on stdout', usage='%(prog)s  --fasta')
+#parser.add_argument('--fasta', help='fasta file in gz for count barcodes')
+#args = parser.parse_args()
+
+def leo_fasta(fasta_in):
+
+    barcodes = {}
+    with gzip.open(fasta_in) as fastq:
+        for line in fastq:
+                if not line.startswith(b'@'): continue
+                bc = line.decode("utf-8").split(':')[-1].strip()
+                if bc not in barcodes:
+                        barcodes[bc] = 1
+                else:
+                        barcodes[bc]+=1
+    return barcodes
+
+
+
+
+
+
+def main(argv):
+    if len(sys.argv) != 1:
+        raise SystemExit(f'Uso adecuado: {sys.argv[0]} fasta file (*.gz)')
+    
+    #informe_camion(sys.argv[1],sys.argv[2],sys.argv[3])
+    aux = leo_fasta(sys.argv[1])
+    total = sum(aux.values())
+    for k, v in sorted(aux.items(), key=itemgetter(1)):
+        print(k, v, round(v/total*100, 2))
+    
+if __name__ == '__main__':
+    main(sys.argv)
